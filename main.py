82a729fcbdae221f9dc19375a70e61a01caf2ff3
Smart developer <developer@yandex.ru>
2022-11-21 20:03:58 +0300

    Написал 2 код

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from random import randint
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from UI import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
@@ -25,7 +26,7 @@ class MyWidget(QMainWindow):
 
     def draw_ellipse(self, qp):
         a = randint(1, 400)
-        qp.setBrush(QColor('yellow'))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         qp.drawEllipse(30, 30, a, a)
 
 
