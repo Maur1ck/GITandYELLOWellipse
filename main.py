d8b496b6e152b7d5b0a01242a20235029b9a8890
Smart developer <developer@yandex.ru>
2022-11-21 19:50:52 +0300

    Написал 1 код

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+from random import randint
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_ellipse(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_ellipse(self, qp):
+        a = randint(1, 400)
+        qp.setBrush(QColor('yellow'))
+        qp.drawEllipse(30, 30, a, a)
+
+
+def except_hook(cls, exception, traceback):
+    sys._excepthook_(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec_())
